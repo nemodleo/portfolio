f2dec801e0ff54ec798081e1e463fe0d2833ba7f
Hyun Park <nemod.leo@snu.ac.kr>
2021-08-30 09:20:33 +0900

    Fix url

--- build2docs.py
+++ build2docs.py
@@ -1,9 +1,13 @@
 #-*- coding: utf-8 -*- 
 import os, glob, shutil 
+from pathlib import Path
 
 def build2docs():
     _, git_repo = os.path.split(os.getcwd())
     print(git_repo)
+
+    if Path("./docs").exists():
+        os.rmdir("./docs")
 
     html = [file for file in glob.glob("build/**", recursive=True) if file.endswith(".html")]
     print(html)
@@ -14,9 +18,9 @@ def build2docs():
         f.close()
         g = open(h, 'w',  encoding='UTF8')
         code = code.replace("/static", "/{}/static".format(git_repo))
-        code = code.replace("/", "/portfolio/")
-        code = code.replace("/experiences/", "/portfolio/experiences/")
-        code = code.replace("/projects/", "/portfolio/projects/")
+        code = code.replace('<a href="/">', '<a href="/{}/">'.format(git_repo))
+        code = code.replace('<a href="/experiences/">', '<a href="/{}/experiences/">'.format(git_repo))
+        code = code.replace('<a href="/projects/">', '<a href="/{}/projects/">'.format(git_repo))
         g.write(code)
         g.close()
 
